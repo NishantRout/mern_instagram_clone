4345f8541795ef1c0457867b590ec9b7dc3279e0
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-10-07 11:28:28 +0530

    api requests

--- mern_insta_backend/server.js
+++ mern_insta_backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import Pusher from "pusher";
+import dbModel from "./dbModel.js";
 
 //app config
 const app = express();
@@ -26,6 +27,28 @@ mongoose.connection.once("open", () => {
 
 //api routes
 app.get("/", (req, res) => res.status(200).send("Hello World!!"));
+
+app.post("/upload", (req, res) => {
+  const body = req.body;
+
+  dbModel.create(body, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+app.get("/sync", (req, res) => {
+  dbModel.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
 
 //listen
 app.listen(port, () => console.log(`Listening on localhost ${port}`));
